9f943f55d0c49a50a3992d1c42983248c2a67a2e
djw <djw@example.com>
2026-09-07 09:51:15 +0800

    服务器部署r、mq

--- app/core/config.py
+++ app/core/config.py
@@ -5,12 +5,12 @@ class Settings(BaseSettings):
     DATABASE_URL: str = "sqlite+aiosqlite:///./database.db"
 
     # Redis 缓存
-    REDIS_URL: str = "redis://127.0.0.1:6379/0"
+    REDIS_URL: str = "redis://8.148.9.192:6379/0"
     CACHE_TTL: int = 60
 
     # Celery
-    CELERY_BROKER_URL: str = "amqp://pyagent:123456@127.0.0.1:5672//"
-    CELERY_RESULT_BACKEND: str = "redis://127.0.0.1:6379/1"
+    CELERY_BROKER_URL: str = "amqp://admin:123456@8.148.9.192:5672//"
+    CELERY_RESULT_BACKEND: str = "redis://8.148.9.192:6379/1"
 
     # JWT
     SECRET_KEY: str = "your-secret-key-change-in-production"
